3f4a3e49096ce95a7036d94ee82a62c31c212197
Mikhail <khomutov.mkhl@gmail.com>
2026-01-16 16:16:00 +0300

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     // Указываем базовый путь для GitHub Pages
-    base: './', // Это создаст относительные пути в сборке
+    base: '/', // Это создаст относительные пути в сборке
 })
