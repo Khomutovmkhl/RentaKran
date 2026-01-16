3eff39e329a0d31d03d482a527d699273707af86
Mikhail <khomutov.mkhl@gmail.com>
2026-01-16 15:37:17 +0300

    Configuring project for deploying on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    // 👇 Ключевая настройка для GitHub Pages:
+    base: process.env.NODE_ENV === 'production' ? '/RentaKran/' : '/',
 })
